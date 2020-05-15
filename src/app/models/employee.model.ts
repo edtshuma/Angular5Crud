082affc0c0510692efe386550da5aafc8b48d7ae
Ed Tshuma <edtshuma@outlook.com>
2020-05-15 03:06:22 +0200

    Add Employee model

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+  id: number;
+  name: string;
+  gender: string;
+  email?: string;
+  phonenumber?: string;
+  contactPreference: string;
+  dateOfBirth: Date;
+  department: string;
+  isActive: Boolean;
+  photoPath?: string;
+}
