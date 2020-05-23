5c2f40c82c27431b9f9f9890ff41200df914090f
Ed Tshuma <edtshuma@outlook.com>
2020-05-23 04:46:20 +0200

    Customize ngx-bootstrap Datepicker
    Min/Max Date, Placement, Default Date , Theming, Control Width

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Department} from '../models/department.model';
+import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 
 
 @Component({
@@ -9,6 +10,8 @@ import {Department} from '../models/department.model';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  dateOfBirth: Date = new Date(2020, 2, 2);
+  datePickerConfig: Partial<BsDatepickerConfig>;
   previewPhoto: false;
   departments: Department [] = [
     {id: 1, name: 'Help Desk'},
@@ -16,7 +19,15 @@ export class CreateEmployeeComponent implements OnInit {
     {id: 3, name: 'Payroll'},
     {id: 4, name: 'Marketing'}
   ];
-  constructor() { }
+  constructor() {
+    this.datePickerConfig = Object.assign({}, {
+      containerClass : 'theme-dark-blue',
+      showWeekNumbers: false,
+      minDate: new Date(2018, 0, 1),
+      maxDate: new Date(2020, 11, 1),
+      dateInputFormat: 'DD/MM/YYYY'
+    });
+  }
 
 
   ngOnInit() {
